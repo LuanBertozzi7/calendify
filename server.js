987e4ed7498a801d4dd83bd9437b9bff624cdd89
LuanBertozzi7 <LuanBertozziS2@hotmail.com>
2025-11-09 16:48:43 -0400

    first commit

--- server.js
+++ server.js
@@ -2,18 +2,24 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 
+const PORT = process.env.PORT || 3000;
 const app = express();
-const PORT = env.PORT || 3000;
-
 app.use(express.json()); // ler as req JSON
 
+app.get("/", (req, res) => {
+  res.redirect(301, "/login.html");
+});
+
+
+// arquivos dentro de /public
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.use(express.static(path.join(__dirname, "public")));
 
+
 app.post("/login", (req, res) => {
   const { username, password } = req.body;
-
-  if (username === "admin" && password === 123) {
+  if (username === "admin" && password === "123") {
+    // Senha deve ser string
     // gera um token aleatório
     const token = Math.random().toString(36).substring(2);
     return res.json({ token });
@@ -22,5 +28,5 @@ app.post("/login", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`server is runing: http://localhost:${PORT}`);
+  console.log(`Server is running: http://localhost:${PORT}`);
 });
